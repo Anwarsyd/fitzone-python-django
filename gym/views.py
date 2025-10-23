ff33ea4ed8142aae46dc41e4262a9ff20c96ade7
anwar <anwarnsyd@gmail.com>
2025-10-23 13:30:17 +0530

    Day 5: Created homepage with some sections

--- gym/views.py
+++ gym/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Slider, Program, Trainer, Faq, Gallery, Testimonial
+from django.views.generic import ListView
 # Create your views here.
 
-def sample(request):
-    return render(request,'base.html')
-# sample
+class HomeView(ListView):
+    template_name = "gym/index.html"
+    queryset = Program.objects.all()
+    context_object_name = 'programs'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sliders"] = Slider.objects.all()
+        context['trainers'] = Trainer.objects.all()
+        context['testimonials'] = Testimonial.objects.all()[:3]
+        return context
+    
